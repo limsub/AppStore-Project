ad731f33f4aeb9f236df7e5d7a18545b09b85400
limsss9076@naver.com <limsss9076@naver.com>
2023-11-12 10:46:28 +0900

    Search 화면 Input, Output 구조

--- AppStoreProject/Scene/Search/SearchAppViewController.swift
+++ AppStoreProject/Scene/Search/SearchAppViewController.swift
@@ -45,6 +45,8 @@ class SearchAppViewController: BaseViewController {
     
     let repository = RealmRepository()
     
+    let viewModel = SearchAppViewModel()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,13 +55,63 @@ class SearchAppViewController: BaseViewController {
         
         repository.printURL()
         
-        bind()
+//        bind()
+        bindInputOutput()
     }
     
-    func bind() {
+    func bindInputOutput() {
         
-        Observable.zip(tableView.rx.itemSelected, tableView.rx.modelSelected(AppInfo.self))
-            .map { return ($0, $1) }
+        let input = SearchAppViewModel.Input(
+            searchButtonClicked: searchController.searchBar.rx.searchButtonClicked,
+            searchText: searchController.searchBar.rx.text.orEmpty,
+            tableViewReachedBottom: tableView.rx.reachedBottom(),
+            tableViewItemSelected: Observable.zip(tableView.rx.itemSelected, tableView.rx.modelSelected(AppInfo.self))
+            )
+        
+        let output = viewModel.transform(input)
+        
+        // 테이블뷰 데이터 구성
+        output.items
+            .bind(to: tableView.rx.items(cellIdentifier: SearchAppTableViewCell.description(), cellType: SearchAppTableViewCell.self )) { (row, element, cell) in
+                
+                self.activityIndicator.stopAnimating()
+                
+                cell.designCell(element)
+                
+                cell.downloadButton.rx.tap
+                    .subscribe(with: self) { owner , _ in
+                        if !cell.isDownloaded {
+                            print("추가")
+                            owner.repository.addApp(element.genres[0], item: AppItemTable(element))
+                        } else {
+                            print("삭제")
+                            owner.repository.deleteApp(item: AppItemTable(element))
+                        }
+                        cell.isDownloaded.toggle()
+                    }
+                    .disposed(by: cell.disposeBag)
+            }
+            .disposed(by: disposeBag)
+        
+        
+        // 네비게이션 타이틀 구성
+        output.dataCnt
+            .map { String($0) }
+            .bind(to: navigationItem.rx.title)
+            .disposed(by: disposeBag)
+        
+        
+        // 바닥 도착해서 페이지네이션
+        output.tableViewReachedBottom
+            .bind(with: self) { owner , _   in
+                owner.activityIndicator.startAnimating()
+//                owner.activityIndicator.stopAnimating()
+            }
+            .disposed(by: disposeBag)
+        
+
+        // 아이템 선택해서 화면 전환
+        output.tableViewItemSelected
             .subscribe(with: self) { owner , data in
                 let vc = DetailViewController()
                 vc.viewModel.appInfo = data.1
@@ -68,99 +120,114 @@ class SearchAppViewController: BaseViewController {
                 owner.tableView.deselectRow(at: data.0, animated: true)
             }
             .disposed(by: disposeBag)
+    }
+    
+    func bind() {
+        
+//        Observable.zip(tableView.rx.itemSelected, tableView.rx.modelSelected(AppInfo.self))
+//            .map { return ($0, $1) }
+//            .subscribe(with: self) { owner , data in
+//                let vc = DetailViewController()
+//                vc.viewModel.appInfo = data.1
+//
+//                owner.navigationController?.pushViewController(vc, animated: true)
+//                owner.tableView.deselectRow(at: data.0, animated: true)
+//            }
+//            .disposed(by: disposeBag)
+        
 
+            
         
-        
-        tableView.rx.reachedBottom()
-            .subscribe(with: self) { owner , _ in
-                owner.activityIndicator.startAnimating()
-                
-                owner.offset += 30
-                
-                BasicAPIManager.appendData(owner.searchText, offset: owner.offset) { response in
-                    switch response {
-                    case .success(let data):
-                        var oldData = try! self.items.value()
-                        oldData.append(contentsOf: data.results)
-                        self.items.onNext(oldData)
-                        
-                        var oldCnt = try! self.resultCnt.value()
-                        oldCnt += data.resultCount
-                        self.resultCnt.onNext(oldCnt)
-                        
-                        
-                    case .failure(let error):
-                        print("error : \(error)")
-                    }
-                    DispatchQueue.main.async {
-                        self.activityIndicator.stopAnimating()
-                    }
-                    
-                }
-            }
-            .disposed(by: disposeBag)
+//        tableView.rx.reachedBottom()
+//            .subscribe(with: self) { owner , _ in
+//                owner.activityIndicator.startAnimating()
+//
+//                owner.offset += 30
+//
+//                BasicAPIManager.appendData(owner.searchText, offset: owner.offset) { response in
+//                    switch response {
+//                    case .success(let data):
+//                        var oldData = try! self.items.value()
+//                        oldData.append(contentsOf: data.results)
+//                        self.items.onNext(oldData)
+//
+//                        var oldCnt = try! self.resultCnt.value()
+//                        oldCnt += data.resultCount
+//                        self.resultCnt.onNext(oldCnt)
+//
+//
+//                    case .failure(let error):
+//                        print("error : \(error)")
+//                    }
+//                    DispatchQueue.main.async {
+//                        self.activityIndicator.stopAnimating()
+//                    }
+//
+//                }
+//            }
+//            .disposed(by: disposeBag)
         
         
         
         // cellForRowAt
-        items
-            .bind(to: tableView.rx.items(cellIdentifier: SearchAppTableViewCell.description(), cellType: SearchAppTableViewCell.self)) { (row, element, cell) in
-                
-                cell.designCell(element)
-                
-                cell.downloadButton.rx.tap
-                    .subscribe(with: self) { owner , _ in
-                        
-                        
-                        if !cell.isDownloaded {
-                            // 추가
-                            print("추가")
-                            owner.repository.addApp(element.genres[0], item: AppItemTable(element))
-                        } else {
-                            // 삭제
-                            print("삭제")
-                            owner.repository.deleteApp(item: AppItemTable(element))
-                        }
-                        
-                        cell.isDownloaded.toggle()
-                    }
-                    .disposed(by: cell.disposeBag)
-            }
-            .disposed(by: disposeBag)
+//        items
+//            .bind(to: tableView.rx.items(cellIdentifier: SearchAppTableViewCell.description(), cellType: SearchAppTableViewCell.self)) { (row, element, cell) in
+//
+//                cell.designCell(element)
+//
+//                cell.downloadButton.rx.tap
+//                    .subscribe(with: self) { owner , _ in
+//
+//
+//                        if !cell.isDownloaded {
+//                            // 추가
+//                            print("추가")
+//                            owner.repository.addApp(element.genres[0], item: AppItemTable(element))
+//                        } else {
+//                            // 삭제
+//                            print("삭제")
+//                            owner.repository.deleteApp(item: AppItemTable(element))
+//                        }
+//
+//                        cell.isDownloaded.toggle()
+//                    }
+//                    .disposed(by: cell.disposeBag)
+//            }
+//            .disposed(by: disposeBag)
         
 
-        let request = searchController.searchBar.rx.searchButtonClicked
-            .throttle(.seconds(1), scheduler: MainScheduler.instance)
-            .withLatestFrom(searchController.searchBar.rx.text.orEmpty) { _, query in
-                return query
-            }
-            .flatMap { value in
-                self.searchText = value
-                return BasicAPIManager.fetchInitialData(value)
-            }
-            .asDriver(onErrorJustReturn: SearchAppModel(resultCount: 0, results: []))
-        // 여러 곳에 연결할 때, share 기능을 활용할 수 있도록 drive로 바꿔주었다.
-        // 기존 타입은 complete, error 이벤트가 발생할 수 있는 Observable이었기 때문에 onErrorJustReturn 으로 예외처리 해준다
-        
-        request
-            .drive(with: self) { owner , value in
-                owner.items.onNext(value.results)
-//                print(value.results[0])
-            }
-            .disposed(by: disposeBag)
-        
-        request
-            .map { $0.resultCount }
-            .drive(with: self) { owner , value in
-                owner.resultCnt.onNext(value)
-            }
-            .disposed(by: disposeBag)
-        
-        resultCnt
-            .map { "\($0)개의 검색 결과"}
-            .bind(to: navigationItem.rx.title)
-            .disposed(by: disposeBag)
-        
+//        let request = searchController.searchBar.rx.searchButtonClicked
+//            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+//            .withLatestFrom(searchController.searchBar.rx.text.orEmpty) { _, query in
+//                return query
+//            }
+//            .flatMap { value in
+//                self.searchText = value
+//                return BasicAPIManager.fetchInitialData(value)
+//            }
+//            .asDriver(onErrorJustReturn: SearchAppModel(resultCount: 0, results: []))
+//        // 여러 곳에 연결할 때, share 기능을 활용할 수 있도록 drive로 바꿔주었다.
+//        // 기존 타입은 complete, error 이벤트가 발생할 수 있는 Observable이었기 때문에 onErrorJustReturn 으로 예외처리 해준다
+//
+//        request
+//            .drive(with: self) { owner , value in
+//                owner.items.onNext(value.results)
+////                print(value.results[0])
+//            }
+//            .disposed(by: disposeBag)
+//
+//        request
+//            .map { $0.resultCount }
+//            .drive(with: self) { owner , value in
+//                owner.resultCnt.onNext(value)
+//            }
+//            .disposed(by: disposeBag)
+//
+//        resultCnt
+//            .map { "\($0)개의 검색 결과"}
+//            .bind(to: navigationItem.rx.title)
+//            .disposed(by: disposeBag)
+//
         
         
     }
--- AppStoreProject/Scene/Search/SearchAppViewModel.swift
+++ AppStoreProject/Scene/Search/SearchAppViewModel.swift
@@ -8,24 +8,92 @@
 import Foundation
 import RxSwift
 import RxCocoa
-//
-//class SearchAppViewModel: ViewModelType {
-//    
-//    
-//    
-//    struct Input {
-//        
-//    }
-//    struct Output {
-//        
-//    }
-//    
-//    
-//    func transform(_ input: Input) -> Output {
-//        <#code#>
-//    }
-//    
-//    
-//    
-//    
-//}
+
+class SearchAppViewModel: ViewModelType {
+
+    let disposeBag = DisposeBag()
+    
+    var offset = 0
+
+    struct Input {
+        let searchButtonClicked: ControlEvent<Void> // searchBar.rx.searchButtonClicked
+        let searchText: ControlProperty<String> // searchBar.rx.text.orEmpty
+        let tableViewReachedBottom: ControlEvent<Void> // tableView.rx.reachedBottom
+
+        let tableViewItemSelected: Observable<(ControlEvent<IndexPath>.Element, ControlEvent<AppInfo>.Element)>
+    }
+
+
+    struct Output {
+        let items: BehaviorSubject<[AppInfo]>
+        let dataCnt: BehaviorSubject<Int>
+        let tableViewItemSelected: Observable<(ControlEvent<IndexPath>.Element, ControlEvent<AppInfo>.Element)>
+        let tableViewReachedBottom: ControlEvent<Void> // tableView.rx.reachedBottom
+//        let nextData: PublishSubject<AppInfo>
+    }
+
+
+    func transform(_ input: Input) -> Output {
+        
+        let items = BehaviorSubject<[AppInfo]>(value: [])  // 테이블뷰에 보여줄 데이터
+        let titleCnt = BehaviorSubject<Int>(value: 0) // 현재 데이터 개수
+    
+        // 검색어를 입력했을 때 -> 전체 변경
+        input.searchButtonClicked
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .withLatestFrom(input.searchText) { _, query in
+                return query
+            }
+            .flatMap { BasicAPIManager.fetchInitialData($0) }
+            .subscribe(with: self) { owner , value in
+                items.onNext(value.results)
+                titleCnt.onNext(value.resultCount)
+                
+                owner.offset = 0;
+            }
+            .disposed(by: disposeBag)
+        
+        
+        
+        // 페이지네이션이 일어날 때 -> 배열 뒤에 추가
+        input.tableViewReachedBottom
+            .withLatestFrom(input.searchText) { _, query in
+                return query
+            }
+            .subscribe(with: self) { owner , value in
+                owner.offset += 30;
+                
+                BasicAPIManager.appendData(value, offset: owner.offset) { response in
+                    switch response {
+                    case .success(let data):
+                        var oldData = try! items.value()
+                        oldData.append(contentsOf: data.results)
+                        items.onNext(oldData)
+                        
+                        var oldCnt = try! titleCnt.value()
+                        oldCnt += data.resultCount
+                        titleCnt.onNext(oldCnt)
+                        
+                    case .failure(let error):
+                        print("error : \(error)")
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        
+        
+
+
+        return Output(
+            items: items,
+            dataCnt: titleCnt,
+            tableViewItemSelected: input.tableViewItemSelected,
+            tableViewReachedBottom: input.tableViewReachedBottom
+        )
+    }
+    
+    
+    
+    
+}
